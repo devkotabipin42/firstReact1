f3ed13f19ad71c7b3dedfdab8fb7ea4a0a4c696e
devkotabipin42 <devkotabipin42@gmail.com>
2025-11-06 11:15:19 +0900

    updated components folder and new UI

--- 04_cards_project/src/App.jsx
+++ 04_cards_project/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {Bookmark} from 'lucide-react'
 
 const App = () => {
   return (
     <div className='Parent'>
-      Hello world
+     <div className='card'>
+        <div className="top">
+          <img src="https://www.designyourway.net/blog/wp-content/uploads/2019/09/arrow.jpg" alt="" />
+          <button>Save <Bookmark /></button>
+        </div>
+        <div className="center">
+
+        </div>
+        <div className="bottom">
+
+        </div>
+     </div>
     </div>
   )
 }
