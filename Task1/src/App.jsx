df58bb51f1e2070ac1afb19c0269ed9e3fcc8e2d
devkotabipin42 <devkotabipin42@gmail.com>
2025-12-22 00:49:54 +0900

    update

---
+++ Task1/src/App.jsx
@@ -0,0 +1,13 @@
+import Menu from './component/Menu'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Menu/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ Task1/src/component/Menu.jsx
@@ -0,0 +1,18 @@
+
+const Menu = () => {
+  return (
+    <div className='flex  gap-2'>
+      DVSY
+      <div className="flex gap-2 ">
+        <p>Designer</p>
+        <p>COLLABS</p>
+        <p>EVENTS</p>
+        <p>BLOG</p>
+        <p>CARD</p>
+        <p>GET IN TOUCH</p>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
